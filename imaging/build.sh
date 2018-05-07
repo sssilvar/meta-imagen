5bd276d8a191e66253093d80cb1ecad65cd52367
Santiago S. Silva R <sssilvar@unal.edu.co>
2018-05-07 13:17:48 -0500

    Structural pattern extraction - Initial version

--- imaging/build.sh
+++ imaging/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script builds the docker image with tag sssilva/eshape:1.0
+# This script builds the docker image with tag sssilvar/eshape:1.0
 # To set the proxy use this format: [http://user:pass@proxy_server.com:port/]
 docker build -t sssilvar/eshape_fs:1.0 \
 	--build-arg proxy=$1\
