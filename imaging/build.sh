34eeff4fd2b8de415cb44b9defb42e41f3d3b3af
santiago smith silva rincon <ssilvari@delorme.inria.fr>
2018-05-09 13:31:40 +0100

    Entrypoint corrected

--- imaging/build.sh
+++ imaging/build.sh
@@ -5,3 +5,33 @@
 docker build -t sssilvar/eshape_fs:1.0 \
 	--build-arg proxy=$1\
 	$(pwd)
+
+# Set parameters up
+CONTAINER_NAME="enigma_shape"
+USER="sssilvar"
+PROXY=$1
+
+IMG_NAME=$USER"/"$CONTAINER_NAME
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+echo "[  OK  ] Stoping container"
+STOP_CONT="docker stop "$CONTAINER_NAME
+eval $STOP_CONT
+
+echo "[  OK  ] Deleting container"
+DEL_CONT="docker rm "$CONTAINER_NAME
+eval $DEL_CONT
+
+echo "[  OK  ] Deleting image"
+DEL_IMG="docker rmi "$IMG_NAME
+eval $DEL_IMG
+
+echo "[  OK  ] Creating the new image: "$IMG_NAME
+CRE_IMG="docker build -t "$IMG_NAME" ."
+eval $CRE_IMG
+
+echo "[  OK  ] Running container: "$CONTAINER_NAME
+cmd="docker run --name "$CONTAINER_NAME" -ti "$IMG_NAME
+echo $cmd
+eval $cmd
