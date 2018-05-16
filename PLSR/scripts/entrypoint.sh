5e284d33326f44ed662f2d6adea8795c020f3fb7
Santiago S. Silva R <sssilvar@unal.edu.co>
2018-05-16 19:02:24 +0200

    ENIGMA docker finished

--- PLSR/scripts/entrypoint.sh
+++ PLSR/scripts/entrypoint.sh
@@ -6,6 +6,6 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo -e "\n\n[  OK  ] Starting statistical analysis"
 
-CMD="python3 "${CURRENT_DIR}"/test.py > "${DATA_FOLDER}"/log.log"
+CMD="python3 "${CURRENT_DIR}"/test_plsr.py > "${DATA_FOLDER}"/log.log"
 eval ${CMD}
 eval "chmod -R 766 "${DATA_FOLDER}
---
+++ PLSR/scripts/test/test_avg_std_iterative.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import argparse
+
+import numpy as np
+import pandas as pd
+
+# Set root folder
+root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(root)
+
+from lib.welford import Welford
+
+if __name__ == '__main__':
+    real_mean = 3.45
+    realt_std = 0.8
+
+    # Generate random data
+    data = np.random.normal(real_mean, realt_std, 30000)
+
+    print("Data shape: ", data.shape)
+    print("Real Mean: ", data.mean())
+    print("Real STD: ", data.std())
+
+    n = 100
+    split = np.split(data, n)
+
+    # Start the iterative calculation
+    wf = Welford()
+    for i in range(n):
+        data_batch = split[i]
+        print(" Iteration ", i + 1)
+        print("\t Batch size:", data_batch.shape)
+
+        wf(data_batch)
+        print(wf)
+
+
+
+
