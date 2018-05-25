63e87f4fb091469a8f0b7ff174f790c0a41d1892
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-05-25 17:05:15 +0200

    recon-all pipeline dockerized started

---
+++ imaging/recon-all/scripts/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo -e "\n\n[  OK  ] Running Entrypoint..."
+python3 /root/scripts/recon_all_multicore.py
---
+++ imaging/recon-all/scripts/recon_all_multicore.py
@@ -0,0 +1,4 @@
+# TODO: Perform a recon-all using multicore and multiple image formats
+
+if __name__ == '__main__':
+    print('Hello Test recon-all')
