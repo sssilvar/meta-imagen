c8ea3be7118a97ea4c15740c95141dcc58fd12ca
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-12 14:59:35 +0200

    ADNI_MCI and MIRIAD Test

--- test/check_results/pca_results.py
+++ test/check_results/pca_results.py
@@ -13,7 +13,31 @@ if __name__ == '__main__':
     features = []
     for i in range(1, n_centers + 1):
         csv = join(main_folder, 'center_%d' % i, 'output', 'groupfile_features.csv')
+        print('[  INFO  ] Loading %s' % csv)
         df = pd.read_csv(csv, index_col=0)
         features.append(df)
+    
     df_feats = pd.concat(features, axis=0)
+
+    # ==== Assign labels ===
+    # Initialize array
+    labels = np.empty(len(df_feats))
+    label_names = np.empty_like(labels)
+
+    adni_prog = pd.read_csv('https://raw.githubusercontent.com/sssilvar/CSD-AD/master/param/data_df.csv', index_col='folder')
+
+    for i, sid in enumerate(df_feats.index):
+        if 'HC' in sid:
+            labels[i] = 0
+            label_names[i] = 'HC'
+        elif 'AD' in sid:
+            labels[i] = 4
+            label_names[i] = 'AD'
+        else:
+            labels[i] = adni_prog.loc[sid, 'target'] + 1
+            label_names[i] = adni_prog.loc[sid, 'dx_group']
+
+    print(label_names)
+    print(len(label_names), df_feats.shape)
+
 
