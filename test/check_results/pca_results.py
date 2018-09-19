7e72efd6060f24741cc59701690a63a604f897b0
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-19 17:54:49 +0200

    3 center test (UK PP AD)

--- test/check_results/pca_results.py
+++ test/check_results/pca_results.py
@@ -8,8 +8,46 @@ from PLSR import PLSR
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
+from sklearn.mixture import GMM
+
+
 # plt.style.use('ggplot')
 sns.set(color_codes=True)
+
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = ax or plt.gca()
+    
+    # Convert covariance to principal axes
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+    
+    # Draw the Ellipse
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height,
+                             angle, **kwargs))
+
+
+def plot_gmm(gmm, X, label=True, ax=None):
+    ax = ax or plt.gca()
+    labels = gmm.fit(X).predict(X)
+    if label:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+    else:
+        ax.scatter(X[:, 0], X[:, 1], s=40, zorder=2)
+    ax.axis('equal')
+    
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, covar, w in zip(gmm.means_, gmm.covars_, gmm.weights_):
+        draw_ellipse(pos, covar, alpha=w * w_factor)
 
 
 
@@ -165,5 +203,13 @@ if __name__ == '__main__':
         # ax.text(x_c[i], y_c[i], key, size=16, color=col)
     plt.xlim(-215, 215)
     plt.ylim(-100, 75)
+
+    # ==== GMM ====
+    Y = result['labels'].astype('category').cat.codes
+    X = result.drop.loc[:, ['PC1', 'PC2']].values
+    
+    gmm = GMM(n_components=4, random_state=42)
+    plot_gmm(gmm, X)
+
     plt.show()
 
