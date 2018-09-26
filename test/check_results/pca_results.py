0b81189a92b009359d405282621344271685033b
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-26 13:51:14 +0200

    ADNI subjects added

--- test/check_results/pca_results.py
+++ test/check_results/pca_results.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
 
         elif any([sid in s for s in adnimerge.index]):
             dx = adnimerge.loc[sid, 'DX']
-            if dx is 'NC'
+            if dx is 'NC':
                 labels[i] = l['HC']
                 label_names[i] = 'HC-ADNI'
             elif dx is 'AD':
