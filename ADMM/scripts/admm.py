3fd7d3286cb37d28aac34084d1d4230c1dd9574c
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-27 13:39:18 +0200

    ADNI subjects added

--- ADMM/scripts/admm.py
+++ ADMM/scripts/admm.py
@@ -288,7 +288,7 @@ def load_data(x_csv, y_csv, fillna=True):
         X = X.fillna(X.mean())
 
     # TODO: REMOVE THIS
-    Y['Age'] = Y['Age'].multiply(Y['Age'])  # Just correcting age^2
+    X['Age'] = X['Age'].multiply(X['Age'])  # Just correcting age^2
     
     return X, Y
 
